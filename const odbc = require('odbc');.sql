d8387197680dd991500befe1b5459d2368e7a3c6
isaac Njoroge <551897@student.embuni.ac.ke>
2026-03-03 13:39:59 +0300

    .gitignore

--- const odbc = require('odbc');.sql
+++ const odbc = require('odbc');.sql
@@ -2,7 +2,7 @@ const odbc = require('odbc');
 
 async function connect() {
   try {
-    const connection = await odbc.connect(`Driver={ODBC Driver 17 for SQL Server};Server=192.168.47.47,1433;Database=procudata;Uid=HP;Pwd=admin;Encrypt=yes;TrustServerCertificate=yes;`);
+    const connection = await odbc.connect(`Driver={ODBC};Server=unique.adress,1433;Database=procudata;Uid=HP;Pwd=admin;Encrypt=yes;TrustServerCertificate=yes;`);
     const result = await connection.query('SELECT * FROM mytable');
     console.log(result);
     await connection.close();
